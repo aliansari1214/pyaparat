3440547140057006bef9c88c31fc652f59ff8004
amirbigg <amirbig44@gmail.com>
2020-04-27 11:39:43 +0430

    initialized repo

---
+++ main.py
@@ -0,0 +1,35 @@
+from scraper import Scraper
+import sys
+import requests
+
+
+class Main:
+	def __init__(self, url, quality):
+		self.url = url
+		self.quality = quality
+		self.scraper = Scraper(url, quality)
+
+	def download(self):
+		video_url = str(self.scraper.get_link())
+		video_name = video_url.split('/')
+		with open(video_name[4], 'wb') as f:
+			print('Downloading...')
+			result = requests.get(video_url, stream=True)
+			total = result.headers.get('content-length')
+
+			if total is None:
+				f.write(result.content)
+			else:
+				download = 0
+				total = int(total)
+				for data in result.iter_content(chunk_size=4096):
+					download += len(data)
+					f.write(data)
+					done = int(50*download / total)
+					sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
+					sys.stdout.flush()
+		print('\nVideo downloaded...')
+
+	def available_qualities(self):
+		return self.scraper.get_qualities()
+
---
+++ scraper.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from exceptions import QualityError
+
+
+qualities = {
+	'144': 0,
+	'240': 1,
+	'360': 2,
+	'480': 3,
+	'720': 4,
+	'1080': 5
+}
+
+
+class Scraper:
+	def __init__(self, url, quality):
+		self.url = url
+		self.quality = quality
+
+	def get_all_links(self):
+		result = requests.get(self.url)
+		content = BeautifulSoup(result.text, 'html.parser')
+		video_links = content.find_all('a', href=re.compile('.mp4'))
+		hrefs = [link['href'] for link in video_links]
+		return hrefs
+
+	def get_link(self):
+		links = self.get_all_links()
+		available_qualities = self.get_qualities()
+		if self.quality not in available_qualities:
+			raise QualityError(f'This quality is not avalable \n available qualities are {available_qualities}')
+		else:
+			link = links[qualities[self.quality]]
+			return link
+
+	def get_qualities(self):
+		links = self.get_all_links()
+		qua = list(qualities.keys())
+		available_qualities = []
+		for i in range(len(links)):
+			available_qualities.append(qua[i])
+		return available_qualities
